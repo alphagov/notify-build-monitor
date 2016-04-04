018864a3ccfba97d4b4cd0797812694af9428b6f
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2016-04-04 13:28:51 +0100

    Try print stmt

--- build_monitor.py
+++ build_monitor.py
@@ -37,6 +37,7 @@ def status():
 
 @app.route('/notifications/sms/mmg', methods=['POST'])
 def temp_mmg_delivery_receipt():
+    print('Posted delivery receipt from mmg: {}'.format(request.json)
     logger.info('Delivery receipt from mmg: {}'.format(request.json))
 
 
